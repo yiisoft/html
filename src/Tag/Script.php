2589796f0fc0772edcee93f0cc50ffa0ae84a78d
Sergey Predvoditelev <sergey.predvoditelev@gmail.com>
2021-02-07 21:11:46 +0300

    Add tag SCRIPT.

---
+++ src/Tag/Script.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Html\Tag;
+
+use Yiisoft\Html\Tag\Base\NormalTag;
+
+/**
+ * @link https://www.w3.org/TR/html52/semantics-scripting.html#the-script-element
+ */
+final class Script extends NormalTag
+{
+    /**
+     * Alias for {@see src}
+     */
+    public function url(?string $url): self
+    {
+        return $this->src($url);
+    }
+
+    /**
+     * @link https://www.w3.org/TR/html52/semantics-scripting.html#element-attrdef-script-src
+     */
+    public function src(?string $url): self
+    {
+        $new = clone $this;
+        $new->attributes['src'] = $url;
+        return $new;
+    }
+
+    /**
+     * @link https://www.w3.org/TR/html52/semantics-scripting.html#element-attrdef-script-type
+     */
+    public function type(?string $type): self
+    {
+        $new = clone $this;
+        $new->attributes['type'] = $type;
+        return $new;
+    }
+
+    /**
+     * @link https://www.w3.org/TR/html52/semantics-scripting.html#element-attrdef-script-charset
+     */
+    public function charset(?string $charset): self
+    {
+        $new = clone $this;
+        $new->attributes['charset'] = $charset;
+        return $new;
+    }
+
+    /**
+     * @link https://www.w3.org/TR/html52/semantics-scripting.html#element-attrdef-script-async
+     */
+    public function async(bool $async = true): self
+    {
+        $new = clone $this;
+        $new->attributes['async'] = $async;
+        return $new;
+    }
+
+    /**
+     * @link https://www.w3.org/TR/html52/semantics-scripting.html#element-attrdef-script-defer
+     */
+    public function defer(bool $defer = true): self
+    {
+        $new = clone $this;
+        $new->attributes['defer'] = $defer;
+        return $new;
+    }
+
+    protected function getName(): string
+    {
+        return 'script';
+    }
+}
---
+++ tests/Tag/ScriptTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Html\Tests\Tag;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Html\Tag\Script;
+
+final class ScriptTest extends TestCase
+{
+    public function testBase(): void
+    {
+        $this->assertSame(
+            '<script src="main.js"></script>',
+            (string)Script::tag()->url('main.js')
+        );
+    }
+
+    public function dataUrl(): array
+    {
+        return [
+            ['<script></script>', null],
+            ['<script src="main.js"></script>', 'main.js'],
+        ];
+    }
+
+    /**
+     * @dataProvider dataUrl
+     */
+    public function testUrl(string $expected, ?string $url): void
+    {
+        $this->assertSame($expected, (string)Script::tag()->url($url));
+        $this->assertSame($expected, (string)Script::tag()->src($url));
+    }
+
+    public function dataType(): array
+    {
+        return [
+            ['<script></script>', null],
+            ['<script type="text/javascript"></script>', 'text/javascript'],
+        ];
+    }
+
+    /**
+     * @dataProvider dataType
+     */
+    public function testType(string $expected, ?string $type): void
+    {
+        $this->assertSame($expected, (string)Script::tag()->type($type));
+    }
+
+    public function dataCharset(): array
+    {
+        return [
+            ['<script></script>', null],
+            ['<script charset="UTF-8"></script>', 'UTF-8'],
+        ];
+    }
+
+    /**
+     * @dataProvider dataCharset
+     */
+    public function testCharset(string $expected, ?string $charset): void
+    {
+        $this->assertSame($expected, (string)Script::tag()->charset($charset));
+    }
+
+    public function testAsync(): void
+    {
+        $this->assertSame('<script async></script>', (string)Script::tag()->async());
+        $this->assertSame('<script></script>', (string)Script::tag()->async(false));
+        $this->assertSame('<script></script>', (string)Script::tag()->async(true)->async(false));
+    }
+
+    public function testDefer(): void
+    {
+        $this->assertSame('<script defer></script>', (string)Script::tag()->defer());
+        $this->assertSame('<script></script>', (string)Script::tag()->defer(false));
+        $this->assertSame('<script></script>', (string)Script::tag()->defer(true)->defer(false));
+    }
+
+    public function testImmutability(): void
+    {
+        $script = Script::tag();
+        $this->assertNotSame($script, $script->url(null));
+        $this->assertNotSame($script, $script->src(null));
+        $this->assertNotSame($script, $script->type(null));
+        $this->assertNotSame($script, $script->charset(null));
+        $this->assertNotSame($script, $script->async());
+        $this->assertNotSame($script, $script->defer());
+    }
+}
