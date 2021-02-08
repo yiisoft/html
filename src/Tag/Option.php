0b966d00e7c8fcace843709a77cb3dfdb732e59e
Sergey Predvoditelev <sergey.predvoditelev@gmail.com>
2021-02-08 21:47:18 +0300

    Add tag OPTION.

---
+++ src/Tag/Option.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Html\Tag;
+
+use Yiisoft\Html\Tag\Base\NormalTag;
+
+/**
+ * @link https://www.w3.org/TR/html52/sec-forms.html#the-option-element
+ */
+final class Option extends NormalTag
+{
+    /**
+     * @link https://www.w3.org/TR/html52/sec-forms.html#element-attrdef-option-value
+     */
+    public function value(?string $value): self
+    {
+        $new = clone $this;
+        $new->attributes['value'] = $value;
+        return $new;
+    }
+
+    /**
+     * @link https://www.w3.org/TR/html52/sec-forms.html#element-attrdef-option-selected
+     */
+    public function selected(bool $selected = true): self
+    {
+        $new = clone $this;
+        $new->attributes['selected'] = $selected;
+        return $new;
+    }
+
+    /**
+     * @link https://www.w3.org/TR/html52/sec-forms.html#element-attrdef-option-disabled
+     */
+    public function disabled(bool $disabled = true): self
+    {
+        $new = clone $this;
+        $new->attributes['disabled'] = $disabled;
+        return $new;
+    }
+
+    protected function getName(): string
+    {
+        return 'option';
+    }
+}
