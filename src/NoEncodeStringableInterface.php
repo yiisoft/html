479335ecb07ad55d6a9282dd9806e7d94e2bb465
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2021-03-15 10:33:52 +0300

    Add description for NoEncodeStringableInterface.

--- src/NoEncodeStringableInterface.php
+++ src/NoEncodeStringableInterface.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Yiisoft\Html;
 
+/**
+ * An object that could be cast to string that should not be HTML-encoded when used.
+ */
 interface NoEncodeStringableInterface
 {
     public function __toString(): string;
