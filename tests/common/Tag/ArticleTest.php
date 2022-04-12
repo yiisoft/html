eaa95aa57fb5dac3677a924e1bfbb3535ca4b1c1
Sameer <soodssr@gmail.com>
2022-04-12 19:44:04 +0530

    [#97] Update Article test with Header and Footer class

--- tests/common/Tag/ArticleTest.php
+++ tests/common/Tag/ArticleTest.php
@@ -5,9 +5,10 @@ declare (strict_types = 1);
 namespace Yiisoft\Html\Tests\Tag;
 
 use PHPUnit\Framework\TestCase;
-use Yiisoft\Html\Html;
 use Yiisoft\Html\Tag\Article;
+use Yiisoft\Html\Tag\Footer;
 use Yiisoft\Html\Tag\H1;
+use Yiisoft\Html\Tag\Header;
 use Yiisoft\Html\Tag\P;
 
 final class ArticleTest extends TestCase
@@ -18,9 +19,9 @@ final class ArticleTest extends TestCase
             '<article><header><h1>Heading 1</h1></header><p>Article content</p><footer>Footer</footer></article>',
             (string) Article::tag()
                 ->content(
-                    Html::tag('header', H1::tag()->content('Heading 1'))
+                    Header::tag()->content(H1::tag()->content('Heading 1'))
                     . P::tag()->content('Article content')
-                    . Html::tag('footer', 'Footer')
+                    . Footer::tag()->content('Footer')
                 )->encode(false)
         );
     }
