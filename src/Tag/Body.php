7505503dd405e51843b73701f2e3848c01e0f5f5
Sameer <soodssr@gmail.com>
2022-04-12 16:44:57 +0530

    [#97] Add Body tag class

---
+++ src/Tag/Body.php
@@ -0,0 +1,21 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Yiisoft\Html\Tag;
+
+use Yiisoft\Html\Tag\Base\NormalTag;
+use Yiisoft\Html\Tag\Base\TagContentTrait;
+
+/**
+ * @link https://html.spec.whatwg.org/multipage/sections.html#the-body-element
+ */
+final class Body extends NormalTag
+{
+    use TagContentTrait;
+
+    protected function getName(): string
+    {
+        return 'body';
+    }
+}
