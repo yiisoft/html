44f37169bbf6cad8c8c6f6fceaa9e5a1e89b2ef2
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2022-04-12 15:38:02 +0300

    Add widget `ButtonGroup`

---
+++ src/Widget/ButtonGroup.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Html\Widget;
+
+use Yiisoft\Html\Html;
+use Yiisoft\Html\NoEncodeStringableInterface;
+use Yiisoft\Html\Tag\Button;
+
+final class ButtonGroup implements NoEncodeStringableInterface
+{
+    private ?string $containerTag = 'div';
+    private array $containerAttributes = [];
+
+    /**
+     * @var Button[]
+     */
+    private array $buttons = [];
+    private array $buttonAttributes = [];
+    private string $separator = "\n";
+
+    public static function create(): self
+    {
+        return new self();
+    }
+
+    public function withoutContainer(): self
+    {
+        return $this->containerTag(null);
+    }
+
+    public function containerTag(?string $name): self
+    {
+        $new = clone $this;
+        $new->containerTag = $name;
+        return $new;
+    }
+
+    public function containerAttributes(array $attributes): self
+    {
+        $new = clone $this;
+        $new->containerAttributes = $attributes;
+        return $new;
+    }
+
+    public function buttons(Button ...$buttons): self
+    {
+        $new = clone $this;
+        $new->buttons = $buttons;
+        return $new;
+    }
+
+    public function buttonAttributes(array $attributes): self
+    {
+        $new = clone $this;
+        $new->buttonAttributes = array_merge($new->buttonAttributes, $attributes);
+        return $new;
+    }
+
+    public function replaceButtonAttributes(array $attributes): self
+    {
+        $new = clone $this;
+        $new->buttonAttributes = $attributes;
+        return $new;
+    }
+
+    public function disabled(?bool $disabled = true): self
+    {
+        $new = clone $this;
+        $new->buttonAttributes['disabled'] = $disabled;
+        return $new;
+    }
+
+    /**
+     * Specifies the form element the buttons belongs to. The value of this attribute must be the ID attribute of a form
+     * element in the same document.
+     *
+     * @param string|null $id ID of a form.
+     *
+     * @link https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#attr-fae-form
+     */
+    public function form(?string $id): self
+    {
+        $new = clone $this;
+        $new->buttonAttributes['form'] = $id;
+        return $new;
+    }
+
+    public function separator(string $separator): self
+    {
+        $new = clone $this;
+        $new->separator = $separator;
+        return $new;
+    }
+
+    public function render(): string
+    {
+        if (empty($this->buttons)) {
+            return '';
+        }
+
+        if (empty($this->buttonAttributes)) {
+            $lines = $this->buttons;
+        } else {
+            $lines = [];
+            foreach ($this->buttons as $button) {
+                $lines[] = $button->attributes($this->buttonAttributes);
+            }
+        }
+
+        $html = [];
+        if (!empty($this->containerTag)) {
+            $html[] = Html::openTag($this->containerTag, $this->containerAttributes);
+        }
+        $html[] = implode($this->separator, $lines);
+        if (!empty($this->containerTag)) {
+            $html[] = Html::closeTag($this->containerTag);
+        }
+
+        return implode("\n", $html);
+    }
+
+    public function __toString(): string
+    {
+        return $this->render();
+    }
+
+    private function __construct()
+    {
+    }
+}
---
+++ tests/common/Widget/ButtonGroupTest.php
@@ -0,0 +1,294 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Html\Tests\Widget;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Html\Html;
+use Yiisoft\Html\Tests\Support\AssertTrait;
+use Yiisoft\Html\Widget\ButtonGroup;
+
+final class ButtonGroupTest extends TestCase
+{
+    use AssertTrait;
+
+    public function testBase(): void
+    {
+        $widget = ButtonGroup::create()
+            ->buttons(
+                Html::resetButton('Reset Data'),
+                Html::submitButton('Send'),
+            );
+
+        $this->assertStringContainsStringIgnoringLineEndings(
+            <<<HTML
+            <div>
+            <button type="reset">Reset Data</button>
+            <button type="submit">Send</button>
+            </div>
+            HTML,
+            $widget->render(),
+        );
+    }
+
+    public function testWithoutButtons(): void
+    {
+        $this->assertSame('', ButtonGroup::create()->render());
+    }
+
+    public function testWithoutContainer(): void
+    {
+        $widget = ButtonGroup::create()
+            ->buttons(
+                Html::resetButton('Reset Data'),
+                Html::submitButton('Send'),
+            )
+            ->withoutContainer();
+
+        $this->assertStringContainsStringIgnoringLineEndings(
+            <<<HTML
+            <button type="reset">Reset Data</button>
+            <button type="submit">Send</button>
+            HTML,
+            $widget->render(),
+        );
+    }
+
+    public function dataContainerTag(): array
+    {
+        return [
+            [
+                '<button type="button">Show</button>',
+                null,
+            ],
+            [
+                '<button type="button">Show</button>',
+                '',
+            ],
+            [
+                <<<HTML
+                <section>
+                <button type="button">Show</button>
+                </section>
+                HTML,
+                'section',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataContainerTag
+     */
+    public function testContainerTag(string $expected, ?string $tagName): void
+    {
+        $widget = ButtonGroup::create()
+            ->buttons(Html::button('Show'))
+            ->containerTag($tagName);
+
+        $this->assertStringContainsStringIgnoringLineEndings($expected, $widget->render());
+    }
+
+    public function testBaseContainerAttributes(): void
+    {
+        $widget = ButtonGroup::create()
+            ->buttons(Html::button('Show'))
+            ->containerAttributes(['id' => 'actions']);
+
+        $this->assertStringContainsStringIgnoringLineEndings(
+            <<<HTML
+            <div id="actions">
+            <button type="button">Show</button>
+            </div>
+            HTML,
+            $widget->render(),
+        );
+    }
+
+    public function testButtonAttributes(): void
+    {
+        $widget = ButtonGroup::create()
+            ->buttons(
+                Html::button('Show'),
+                Html::button('Hide'),
+            )
+            ->buttonAttributes(['class' => 'base']);
+
+        $this->assertStringContainsStringIgnoringLineEndings(
+            <<<HTML
+            <div>
+            <button type="button" class="base">Show</button>
+            <button type="button" class="base">Hide</button>
+            </div>
+            HTML,
+            $widget->render(),
+        );
+    }
+
+    public function testMergeButtonAttributes(): void
+    {
+        $widget = ButtonGroup::create()
+            ->buttons(
+                Html::button('Show'),
+                Html::button('Hide'),
+            )
+            ->buttonAttributes(['class' => 'base'])
+            ->buttonAttributes(['data-key' => '42'])
+            ->disabled();
+
+        $this->assertStringContainsStringIgnoringLineEndings(
+            <<<HTML
+            <div>
+            <button type="button" class="base" disabled data-key="42">Show</button>
+            <button type="button" class="base" disabled data-key="42">Hide</button>
+            </div>
+            HTML,
+            $widget->render(),
+        );
+    }
+
+    public function testReplaceButtonAttributes(): void
+    {
+        $widget = ButtonGroup::create()
+            ->buttons(
+                Html::button('Show'),
+                Html::button('Hide'),
+            )
+            ->disabled()
+            ->replaceButtonAttributes(['class' => 'base']);
+
+        $this->assertStringContainsStringIgnoringLineEndings(
+            <<<HTML
+            <div>
+            <button type="button" class="base">Show</button>
+            <button type="button" class="base">Hide</button>
+            </div>
+            HTML,
+            $widget->render(),
+        );
+    }
+
+    public function dataDisabled(): array
+    {
+        return [
+            [
+                <<<HTML
+                <button type="button">Show</button>
+                <button type="button">Hide</button>
+                HTML,
+                null,
+            ],
+            [
+                <<<HTML
+                <button type="button">Show</button>
+                <button type="button">Hide</button>
+                HTML,
+                false,
+            ],
+            [
+                <<<HTML
+                <button type="button" disabled>Show</button>
+                <button type="button" disabled>Hide</button>
+                HTML,
+                true,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataDisabled
+     */
+    public function testDisabled(string $expected, ?bool $disabled): void
+    {
+        $widget = ButtonGroup::create()
+            ->buttons(
+                Html::button('Show'),
+                Html::button('Hide'),
+            )
+            ->disabled($disabled)
+            ->withoutContainer();
+
+        $this->assertStringContainsStringIgnoringLineEndings($expected, $widget->render());
+    }
+
+    public function dataForm(): array
+    {
+        return [
+            [
+                <<<HTML
+                <button type="button">Show</button>
+                <button type="button">Hide</button>
+                HTML,
+                null,
+            ],
+            [
+                <<<HTML
+                <button type="button" form="CreatePost">Show</button>
+                <button type="button" form="CreatePost">Hide</button>
+                HTML,
+                'CreatePost',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataForm
+     */
+    public function testForm($expected, ?string $id): void
+    {
+        $widget = ButtonGroup::create()
+            ->buttons(
+                Html::button('Show'),
+                Html::button('Hide'),
+            )
+            ->form($id)
+            ->withoutContainer();
+
+        $this->assertStringContainsStringIgnoringLineEndings($expected, $widget->render());
+    }
+
+    public function testSeparator(): void
+    {
+        $widget = ButtonGroup::create()
+            ->buttons(
+                Html::button('Show'),
+                Html::button('Hide'),
+            )
+            ->separator('-');
+
+        $this->assertStringContainsStringIgnoringLineEndings(
+            <<<HTML
+            <div>
+            <button type="button">Show</button>-<button type="button">Hide</button>
+            </div>
+            HTML,
+            $widget->render(),
+        );
+    }
+
+    public function testStringable(): void
+    {
+        $widget = ButtonGroup::create()
+            ->buttons(
+                Html::resetButton('Reset Data'),
+                Html::submitButton('Send'),
+            );
+
+        $this->assertStringContainsStringIgnoringLineEndings($widget->render(), (string) $widget);
+    }
+
+    public function testImmutability(): void
+    {
+        $widget = ButtonGroup::create();
+
+        $this->assertNotSame($widget, $widget->withoutContainer());
+        $this->assertNotSame($widget, $widget->containerTag(null));
+        $this->assertNotSame($widget, $widget->containerAttributes([]));
+        $this->assertNotSame($widget, $widget->buttons());
+        $this->assertNotSame($widget, $widget->buttonAttributes([]));
+        $this->assertNotSame($widget, $widget->replaceButtonAttributes([]));
+        $this->assertNotSame($widget, $widget->disabled());
+        $this->assertNotSame($widget, $widget->form(null));
+        $this->assertNotSame($widget, $widget->separator(''));
+    }
+}
