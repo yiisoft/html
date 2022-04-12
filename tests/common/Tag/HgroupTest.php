93886202a52fd84ac2df9676afaa7f3b45940f1e
Sameer <soodssr@gmail.com>
2022-04-12 19:33:22 +0530

    [#97] Add Hgroup tag class

---
+++ tests/common/Tag/HgroupTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Yiisoft\Html\Tests\Tag;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Html\Tag\H1;
+use Yiisoft\Html\Tag\H2;
+use Yiisoft\Html\Tag\H3;
+use Yiisoft\Html\Tag\Hgroup;
+
+final class HgroupTest extends TestCase
+{
+    public function testBase(): void
+    {
+        $this->assertSame(
+            '<hgroup><h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3></hgroup>',
+            (string) Hgroup::tag()->content(
+                H1::tag()->content('Heading 1')
+                . H2::tag()->content('Heading 2')
+                . H3::tag()->content('Heading 3')
+            )->encode(false)
+        );
+    }
+}
