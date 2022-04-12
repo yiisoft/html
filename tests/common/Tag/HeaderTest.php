62756c02c82b259dca8f64908386fba73ae7e65e
Sameer <soodssr@gmail.com>
2022-04-12 19:39:42 +0530

    [#97] Add Header tag class

---
+++ tests/common/Tag/HeaderTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Yiisoft\Html\Tests\Tag;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Html\Tag\H1;
+use Yiisoft\Html\Tag\Header;
+use Yiisoft\Html\Tag\I;
+
+final class HeaderTest extends TestCase
+{
+    public function testBase(): void
+    {
+        $this->assertSame(
+            '<header><h1>Heading 1</h1><i>Hello Text</i></header>',
+            (string) Header::tag()
+                ->content(
+                    H1::tag()->content('Heading 1')
+                    . I::tag()->content('Hello Text')
+                )->encode(false)
+        );
+    }
+}
