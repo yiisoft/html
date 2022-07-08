a3a967ffeda34dda313259a854973540b787e3a2
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-07-08 10:38:40 +0300

    Update src/Tag/Base/Tag.php
    Co-authored-by: Alexey Rogachev <arogachev90@gmail.com>

--- src/Tag/Base/Tag.php
+++ src/Tag/Base/Tag.php
@@ -93,7 +93,7 @@ abstract class Tag implements NoEncodeStringableInterface
      *
      * @return static
      *
-     * @deprecated Use {@see addClass()} or {@see replaceClass} instead. Please note that in next major
+     * @deprecated Use {@see addClass()} or {@see replaceClass()} instead. Please note that in next major
      * version method `replaceClass()` will be renamed to `class()`.
      */
     final public function class(?string ...$class): self
