67a3ca8c1bc35a27aaed0c93e917806274764f7a
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-10-28 12:58:19 +0300

    `NoEncodeStringableInterface` extend `Stringable` interface

--- src/NoEncodeStringableInterface.php
+++ src/NoEncodeStringableInterface.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Yiisoft\Html;
 
+use Stringable;
+
 /**
  * An object that could be cast to string that should not be HTML-encoded when used.
  */
-interface NoEncodeStringableInterface
+interface NoEncodeStringableInterface extends Stringable
 {
     public function __toString(): string;
 }
