160792a3734696bc74b1eaf1132b65b956288e30
Sergey F <sims2planet@mail.ru>
2022-12-08 18:24:59 +0300

    Add $attributes parameter to Html::img()

--- src/Html.php
+++ src/Html.php
@@ -531,6 +531,7 @@ final class Html
      *
      * @param string|null $url The image URL.
      * @param string|null $alt Alt text.
+     * @param array $attributes The tag attributes in terms of name-value pairs.
      */
     public static function img(?string $url = null, ?string $alt = '',  array $attributes = []): Img
     {
