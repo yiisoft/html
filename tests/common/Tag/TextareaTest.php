0b352252afa421470d4557d0d8770b30dacb3f83
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-08-08 12:19:43 +0300

    improve test

--- tests/common/Tag/TextareaTest.php
+++ tests/common/Tag/TextareaTest.php
@@ -85,7 +85,7 @@ final class TextareaTest extends TestCase
 
                 d</textarea>
                 HTML,
-                ['a', 'b', null, 'c', '', 'd']
+                ['a', new StringableObject('b'), null, 'c', '', 'd']
             ],
         ];
     }
