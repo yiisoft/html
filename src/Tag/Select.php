0f5981893efbb964c93d8d19e55b6dbae730d178
vjik <vjik@users.noreply.github.com>
2024-09-18 07:04:01 +0000

    Apply Rector changes (CI)

--- src/Tag/Select.php
+++ src/Tag/Select.php
@@ -50,9 +50,7 @@ final class Select extends NormalTag
     {
         $new = clone $this;
         $new->values = array_map(
-            static function (Stringable|bool|float|int|string|BackedEnum $v): string {
-                return (string) ($v instanceof BackedEnum ? $v->value : $v);
-            },
+            static fn(Stringable|bool|float|int|string|BackedEnum $v): string => (string) ($v instanceof BackedEnum ? $v->value : $v),
             array_values($value)
         );
         return $new;
